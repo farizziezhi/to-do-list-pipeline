8dee21922af067a36b2243de0be8f14543886da5
aqilfathurrahman <aqiel011@gmail.com>
2025-11-19 15:00:26 +0800

    ini kodenya

---
+++ tests/to-do_test.py
@@ -0,0 +1,9 @@
+from todo import tambah_kegiatan
+
+def test_tambah_kegiatan():
+    daftar_kegiatan = []
+    result = test_tambah_kegiatan(daftar_kegiatan, "Belajar CI")
+    assert len(result) == 1
+
+def test_SelaluGagal ():
+    assert False
