1623199c33e1d69a4cd67cf493a3b3c537800837
muh.farizzi <ezhifarizzi@gmail.com>
2025-11-19 15:12:57 +0800

    Pasti Sukses

--- tests/to-do_test.py
+++ tests/to-do_test.py
@@ -1,6 +1,8 @@
+from todo import tambah_kegiatan
+
 def test_tambah_kegiatan():
     daftar_kegiatan = []
-    result = test_tambah_kegiatan(daftar_kegiatan, "Belajar CI")
+    result = tambah_kegiatan(daftar_kegiatan, "Belajar CI")
     assert len(result) == 1
 
 def test_SelaluGagal():
