a32e492dc78c8171a30898ec1229eb2023ca8591
muh.farizzi <ezhifarizzi@gmail.com>
2025-11-19 15:08:06 +0800

    Cek

--- tests/to-do_test.py
+++ tests/to-do_test.py
@@ -5,5 +5,5 @@ def test_tambah_kegiatan():
     result = test_tambah_kegiatan(daftar_kegiatan, "Belajar CI")
     assert len(result) == 1
 
-def test_SelaluGagal ():
+def test_SelaluGagal():
     assert False
